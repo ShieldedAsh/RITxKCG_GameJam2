8e7af700c6e8921b36c3310f97d03ca7269a661f
Vinny Keeler <vinny@shash.dev>
2026-05-30 13:55:20 +0900

    English keywords recognized and basic attack framework laid out

--- Assets/Scripts/SpeechManager.cs
+++ Assets/Scripts/SpeechManager.cs
@@ -1,3 +1,6 @@
+//DO NOT USE, USE DICTATIONMANAGER.CS
+//使用しないでください。DICTATIONMANAGER.CSを使用してください。
+
 using System;
 using System.Text;
 using UnityEngine;
@@ -12,18 +15,43 @@ public class SpeechManager : MonoBehaviour
 
     void Start()
     {
-        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        //keywords to be recognized, add japanese ones later
+        m_Keywords = new string[] { "boom", "zap", "blaze", "slash", "whoosh", "spring"}; 
+        m_Recognizer = new KeywordRecognizer(m_Keywords, ConfidenceLevel.Low); //low confidence to be more forgiving
         m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
         m_Recognizer.Start();
         Debug.Log(m_Recognizer.IsRunning);
     }
 
     private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
-    {
+    {    
         StringBuilder builder = new StringBuilder();
         builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
         builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
         builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
         Debug.Log(builder.ToString());
+
+        //Different attacks
+        switch(args.text)
+        {
+            case "boom":
+                Attacks.LaunchAttack(Attack.Boom);
+                break;
+            case "zap":
+                Attacks.LaunchAttack(Attack.Zap);
+                break;
+            case "blaze":
+                Attacks.LaunchAttack(Attack.Blaze);
+                break;
+            case "slash":
+                Attacks.LaunchAttack(Attack.Slash);
+                break;
+            case "whoosh":
+                Attacks.LaunchAttack(Attack.Whoosh);
+                break;
+            case "Spring":
+                Attacks.LaunchAttack(Attack.Spring);
+                break;
+        }
     }
 }
