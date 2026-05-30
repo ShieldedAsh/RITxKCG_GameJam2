624494f9d48918752551681fd76572d183cf97eb
sakuraitowa <yingjingxianghe@gmail.com>
2026-05-30 11:50:25 +0900

    Create Tower
    タワーの作成

---
+++ Assets/Towa/Tower.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // タワーの情報
+    [Header("クプリクタブルオブジェクト")]
+    [SerializeField]private TowerStatu statu;
+    // 最大HP
+    private int maxHp;
+    // 現在HP
+    private int hp;
+
+    // 塔の画像変換クラス
+    [Header("画像変更クラス")]
+    [SerializeField] private TowerImageChanger changer;
+
+    void Start()
+    {
+        Initialize();
+
+    }
+    /// <summary>
+    ///  初期化
+    /// </summary>
+    private void Initialize()
+    {
+        //タワーの体力を初期化
+        hp = statu.TowerMaxHp;
+        hp = maxHp;
+    }
+
+    /// <summary>
+    /// ダメージを食らった時の処理
+    /// </summary>
+    /// <param name="damage">ダメージ値</param>
+    public void TakeDamage(int damage) 
+    {
+        hp -= damage;
+
+        // スプライトの更新
+        changer.UpdateSprite(hp);
+    }
+}
