5029eb4cc8c0dd7d75f675aa66cafd61f9092a5e
sakuraitowa <yingjingxianghe@gmail.com>
2026-05-31 14:51:02 +0900

    時間を追加

--- Assets/Towa/Sprict/TimerText.cs
+++ Assets/Towa/Sprict/TimerText.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 
 public class TimerText : MonoBehaviour
 {
-    [SerializeField]
-    TextMeshProUGUI tmp;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshProUGUI tmp;
+
     void Start()
     {
         float time = Tower.lastTime;
-        tmp.text = (int)(time / 60) + ":" + (int)(time % 60); int m = (int)(time / 60);
+
+        int m = (int)(time / 60);
         int s = (int)(time % 60);
 
-        tmp.text = m.ToString("00") + ":" + s.ToString("00");
+        tmp.text = $"{m:00}:{s:00}";
     }
-
 }
